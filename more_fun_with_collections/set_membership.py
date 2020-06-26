0d183709b0f8f8f56eaf98c139d9148b1c467da7
rachelli <brli@dmacc.edu>
2020-06-26 12:11:00 -0500

    finished code

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,8 +1,9 @@
-def in_set():
+def in_set(element, my_set):
     #accept set
+    my_set = {34, 25, 67, 98, 3, 5}
     #return boolean value stating if element is in the set
-
-    pass
+    return element in my_set
 
 if __name__ == '__main__':
-    pass
+    check_set = (34, (34, 25, 67, 98, 3, 5))
+    print(check_set)
