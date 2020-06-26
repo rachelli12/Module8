19ffad5a846bd38bf15aac7b300c35e4088af9a8
rachelli <brli@dmacc.edu>
2020-06-26 14:11:00 -0500

    test passed

--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -4,9 +4,9 @@ from more_fun_with_collections import dict_membership as dm
 
 class TestDict(unittest.TestCase):
     def test_in_dict_true(self):
-        self.assertTrue(dm.in_dict())
+        self.assertTrue(dm.in_dict('A', {'A': 'apple', 'B': 'banana', 'C': 'cantaloupe', 'D': 'dragonfruit'}))
     def test_in_dict_flase(self):
-        self.assertFalse(dm.in_dict())
+        self.assertFalse(dm.in_dict('F', {'A': 'apple', 'B': 'banana', 'C': 'cantaloupe', 'D': 'dragonfruit'}))
 
 if __name__ == '__main__':
     unittest.main()
