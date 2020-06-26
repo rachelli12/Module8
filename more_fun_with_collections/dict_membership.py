99e91c3110c426c2c76d54da2cc77cae117b8628
rachelli <brli@dmacc.edu>
2020-06-26 14:15:04 -0500

    added docstrings

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -1,4 +1,18 @@
+"""
+Program name: dict_membership.py
+Author name: Rachel Li
+Last date modified: 06/25/2020
+
+The purpose of this program is to accept a set
+and return boolean value if element in set.
+"""
 def in_dict(element, my_dict):
+    '''
+    use reST style
+    :param element: this represents the item searched
+    :param my_dict: this repesents the dictionary set
+    :return:  this returns boolean value if item is in set
+    '''
     #accept a set
     my_dict = {'A': 'apple', 'B': 'banana', 'C': 'cantaloupe', 'D': 'dragonfruit'}
     #return boolean value if the element is in the set
