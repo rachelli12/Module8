b228426f3fe26674ba0c5fba412a54b50bf25eeb
rachelli <brli@dmacc.edu>
2020-06-27 14:54:41 -0500

    initial commit. test fail

---
+++ more_fun_with_collections/date_time.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+#lastest birthday
+
+def half_birthday():
+    #returns the half birthday
+    #use timedelta
+    pass
+
+if __name__ == '__main__':
+    oTime = datetime.now()
+    print(oTime.isoformat())
+    pass
+
