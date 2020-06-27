925de53222cb98491b1a8bd32ec912b4c0372236
rachelli <brli@dmacc.edu>
2020-06-27 15:34:13 -0500

    test passed

--- test/test_date_time.py
+++ test/test_date_time.py
@@ -4,7 +4,9 @@ from more_fun_with_collections import date_time as dt
 
 class TestDate(unittest.TestCase):
     def test_half_birthday(self):
-        self.assertEqual(dt.half_birthday(2020, 9, 9), 2021, 3, 26)
+        birthdate = '2020-09-24'
+        expected_date = '2021-03-26'
+        self.assertEqual(expected_date, dt.half_birthday(birthdate))
 
 if __name__ == '__main__':
     unittest.main()
