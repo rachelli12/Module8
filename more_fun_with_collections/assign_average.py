8ed420475f2d0f6c6975e811b099be0cbe54c038
rachelli <brli@dmacc.edu>
2020-06-28 12:19:11 -0500

    added docstrings and reST style

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,4 +1,20 @@
+"""
+Program name: assign_average.py
+Author: Rachel Li
+Last date modified: 06/28/2020
+
+The purpose of this program is to
+assign a grade to average scores
+and print GPA based on average score.
+"""
+
 def grade(average_score):
+    '''
+    use reST style
+
+    :param average_score: this represents the average
+    :return: this returns the average
+    '''
     returned_score = 0.0
 
 def one():
@@ -17,6 +33,16 @@ def default():
     return 'F'
 
 def switch_average(score1, score2, score3):
+    '''
+    use reST style
+
+    :param score1: this represents user input score1
+    :param score2: this represents user input score2
+    :param score3: this represents user input score3
+    :param average_score: this represents the average of the test scores
+    :return: returns the average_score
+    :keyError: raise ValueError
+    '''
     total = 0.0
     average_score = int(score1 + score2 + score3) / 3
     if 90 <= average_score <= 100:
@@ -35,7 +61,13 @@ def switch_average(score1, score2, score3):
     #make similar function wise to a switch statement
     #useing a dictionary and if, elif, else
 
-def switch_grades(grades):
+def switch_gpa(grades):
+    '''
+    use reST style
+
+    :param grades: this represents the average grade
+    :return: returns the GPA based on the grade
+    '''
     score_dict = {
     one(): 4.0,
     two(): 3.0,
@@ -52,6 +84,6 @@ if __name__ == '__main__':
     score2 = int(input("Enter score2: "))
     score3 = int(input("Enter score3: "))
     average_score = switch_average(score1, score2, score3)
-    print(switch_grades(average_score))
+    print(switch_gpa(average_score))
     print(average_score)
     grade(average_score)
