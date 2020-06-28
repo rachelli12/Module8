c4433549f752762d6b4321fb8bc75ac023ba9392
rachelli <brli@dmacc.edu>
2020-06-28 12:10:49 -0500

    fixed grading. left out range of numbers

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -25,9 +25,9 @@ def switch_average(score1, score2, score3):
         return two()
     elif 70 <= average_score < 80:
         return three()
-    elif 50 <= average_score < 60:
+    elif 50 < average_score < 70:
         return four()
-    elif average_score < 50:
+    elif average_score <= 50:
         return default()
     elif average_score > 100 and average_score < 0:
         print("Enter valid input")
