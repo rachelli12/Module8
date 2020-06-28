32c5fa445dd3e8116f35af665e7d52061bfaabcd
rachelli <brli@dmacc.edu>
2020-06-28 11:34:31 -0500

    initial commit

---
+++ more_fun_with_collections/assign_average.py
@@ -0,0 +1,33 @@
+def switch_average():
+    #return score = ??
+    #make similar function wise to a switch statement
+    #useing a dictionary and if, elif, else
+    #
+    pass
+
+def score_to_grade():
+    pass
+1. variableToCompare = 'January'
+2. switch( variableToCompare ) {
+3.     case 'September': month = 9
+4.         break
+5.     case 'November': month = 11
+6.         break
+7.     case s: ...statements...
+8.         break
+9.     case 'January': month = 1
+10.         break
+11.    default: raise ValueError
+12.        break
+13. }
+
+a_dict = {
+'A': 4.0
+'B': 3.0
+'C': 2.0
+'D': 1.0
+'F': 0.0
+}
+
+if __name__ == '__main__':
+    pass
