4fcb42785fff842174050ea97a2f81ccbb70e051
rachelli <brli@dmacc.edu>
2020-06-28 12:08:09 -0500

    3 test passed

--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -3,20 +3,20 @@ from more_fun_with_collections import assign_average as aa
 
 class TestGrade(unittest.TestCase):
     def test_average_grade_a(self):
-        average = 90
-        self.assertEqual(average, aa.switch_average(score1 = 90, score2 = 90, score3 = 90))
+        average_grade = 'A'
+        self.assertEqual(average_grade, aa.switch_average(score1 = 90, score2 = 90, score3 = 90))
     def test_average_grade_b(self):
-        average = 80
-        self.assertEqual(average, aa.switch_average(score1 = 80, score2 = 80, score3 = 80))
+        average_grade = 'B'
+        self.assertEqual(average_grade, aa.switch_average(score1 = 80, score2 = 80, score3 = 80))
     def test_average_grade_c(self):
-        average = 70
-        self.assertEqual(average, aa.switch_average(score1 = 70, score2 = 70, score3= 70))
+        average_grade = 'C'
+        self.assertEqual(average_grade, aa.switch_average(score1 = 70, score2 = 70, score3= 70))
     def test_average_grade_d(self):
-        average = 60
-        self.assertEqual(average, aa.switch_average(score1 = 60, score2 = 60, score3 = 60))
+        average_grade = 'D'
+        self.assertEqual(average_grade, aa.switch_average(score1 = 60, score2 = 60, score3 = 60))
     def test_average_grade_f(self):
-        average = 50
-        self.assertEqual(average, aa.switch_average(score1 = 50, score2 = 50, score3 = 50))
+        average_grade = 'F'
+        self.assertEqual(average_grade, aa.switch_average(score1 = 49, score2 = 49, score3 = 49))
     def test_average_invalid(self):
         with self.assertRaises(ValueError):
             aa.switch_average(score1= 101, score2= 101, score3 = 101)
